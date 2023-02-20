7acc922f3867d5f524aa52b9c02661dac8549deb
Isadora Souza <isadora_msouza@hotmail.com>
2023-02-20 19:52:56 -0300

    Add parameter store service

---
+++ spring-cloud-localstack/spring-cloud-localstack/src/main/java/com/example/springcloudlocalstack/SpringCloudLocalstackApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloudlocalstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudLocalstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudLocalstackApplication.class, args);
+	}
+
+}
---
+++ spring-cloud-localstack/spring-cloud-localstack/src/test/java/com/example/springcloudlocalstack/SpringCloudLocalstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudlocalstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudLocalstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
